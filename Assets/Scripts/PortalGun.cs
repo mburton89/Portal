a4131abcf29b2aa5e6973047bf3e6e37e830e876
Matt Burton <matthewwburton@gmail.com>
2020-10-14 09:35:48 -0500

    Gutted and Polished

--- Assets/Scripts/PortalGun.cs
+++ Assets/Scripts/PortalGun.cs
@@ -6,33 +6,33 @@ public class PortalGun : MonoBehaviour
 {
     public AudioSource portalSound;
     public AudioSource errorSound;
-	public GameObject orangePortal;
-	public GameObject bluePortal;
-	public GameObject gunTip;
-	
-	// Update is called once per frame
-	void Update ()
+    public GameObject orangePortal;
+    public GameObject bluePortal;
+    public GameObject firingPoint;
+
+    // Update is called once per frame
+    void Update()
     {
-		// fire the right portal (left or right) based on input
-		if (Input.GetMouseButtonDown(0))
+        // fire the right portal (left or right) based on input
+        if (Input.GetMouseButtonDown(0))
         {
-			FirePortal(true);
-		}
+            FirePortal(false);
+        }
         else if (Input.GetMouseButtonDown(1))
         {
-			FirePortal(false);
-		}
-	}
+            FirePortal(true);
+        }
+    }
 
-	void FirePortal(bool isOrange)
+    void FirePortal(bool isOrange)
     {
-		// struct object that will hold our raycast information
-		RaycastHit hit;
+        // struct object that will hold our raycast information
+        RaycastHit raycastHit;
 
-		// if we collide with an object with our raycast, spawn a portal there
-		if (Physics.Raycast(gunTip.transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+        // if we collide with an object with our raycast, spawn a portal there
+        if (Physics.Raycast(firingPoint.transform.position, transform.TransformDirection(Vector3.forward), out raycastHit, Mathf.Infinity))
         {
-			portalSound.Play();
+            portalSound.Play();
 
             // choose between the correct portals based on bool input
             if (isOrange)
@@ -40,16 +40,16 @@ public class PortalGun : MonoBehaviour
                 // set the portal to the same position as the raycast point, and set
                 // its rotation to orient to the wall relative to what its "up" direction is,
                 // which is Vector.up in world space 
-                orangePortal.transform.SetPositionAndRotation(hit.point, Quaternion.FromToRotation(Vector3.forward, hit.normal));
+                orangePortal.transform.SetPositionAndRotation(raycastHit.point, Quaternion.FromToRotation(Vector3.forward, raycastHit.normal));
             }
             else
             {
-                bluePortal.transform.SetPositionAndRotation(hit.point, Quaternion.FromToRotation(Vector3.forward, hit.normal));
+                bluePortal.transform.SetPositionAndRotation(raycastHit.point, Quaternion.FromToRotation(Vector3.forward, raycastHit.normal));
             }
-		}
+        }
         else
         {
-			errorSound.Play();
-		}
-	}
+            errorSound.Play();
+        }
+    }
 }
